05547151aee58cff6b860d605ea07908be3fd33c
adriana <acrugeles@uninorte.edu.co>
2018-10-21 18:14:51 -0500

    ya puse las dependecias en el script

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo script
+echo Adriana Rugeles, Andres Concha y Maximiliam Garcia
+echo configuring hosts
+apt-get install -y build-essential
+apt-get install -y openssh-server
+apt-get install -y nfs-kernel-server
+apt-get install -y nfs-common 
